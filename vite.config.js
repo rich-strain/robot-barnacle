beb441e12bed531acd8407a527f51c0770959939
Rich <richardstrain@gmail.com>
2024-10-14 16:50:39 -0400

    Vite Config Update

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
+  sbuild: {
+    outDir: 'dist', // Ensure the build output directory is 'dist'
   },
+  server: {
+    open: true, // Automatically open the app in the browser on start
+  },
+  base: '/', // Set the base URL
 });
